cb55973bf8d9fd90dd310563d22657f4b7e5128c
karen <karen.tumanyan97@gmail.com>
2018-01-14 02:15:39 +0400

    Add port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send('Hello Wolrd');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('server start on port 3000');
 });
